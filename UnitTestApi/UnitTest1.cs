b73fde58d4c36d3c92647ea720de7325248aef05
adrie <adriengirard@yahoo.fr>
2022-06-29 10:33:31 +0200

    add unit test

--- UnitTestApi/UnitTest1.cs
+++ UnitTestApi/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using ApiControler;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,8 +12,44 @@ namespace UnitTestApi
         {
             var p = new APIcontrol();
             p.GetCity("48.864716","2.349014");
-            Assert.AreEqual(p.objectRes.name,"Paris","Error Api City");
+            Assert.AreEqual(p.objectRes.name,"Palais-Royal","Error Api City");
             Assert.AreNotEqual(p.objectRes.name,"gggg","Error Api City");
+
+        }
+        
+        [TestMethod]
+        public void testCity2()
+        {
+            var p = new APIcontrol();
+            p.GetCity("59.9138688","10.7522454");
+            Assert.AreEqual(p.objectRes.name,"Oslo","Error Api City");
+            Assert.AreNotEqual(p.objectRes.name,"Paris","Error Api City");
+
+        }
+        [TestMethod]
+        public void testCity3()
+        {
+            var p = new APIcontrol();
+            p.GetInfo2("Paris");
+            Assert.AreEqual(p.objectRes.name,"Paris","Error Api City");
+            Assert.AreNotEqual(p.objectRes.name,"Prague","Error Api City");
+
+        }
+        
+        [TestMethod]
+        public void testForecast()
+        {
+            var p = new APIcontrol();
+            p.GetInfo2("Paris");
+            Assert.AreNotEqual(p.objectRes2.list[8].dt,p.objectRes2.list[16].dt,"Error Api City");
+
+        }
+        [TestMethod]
+        public void testForecast2()
+        {
+            var p = new APIcontrol();
+            p.GetInfo2("cetteVillen'existepas");
+            Assert.AreEqual(p.objectRes.name,"Paris","Error Api City");
 
         }
     }
